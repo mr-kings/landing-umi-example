d686fb5f29568747a03f7dea3eb18d1e0123c67a
kim <1134771121@qq.com>
2019-12-08 00:33:31 +0800

    feat:加ico

--- src/api/sendEmail.js
+++ src/api/sendEmail.js
@@ -2,7 +2,7 @@ import { request } from '@/api';
 
 // 发送邮件
 export const sendEmail = (params) => {
-  return request.POST('http://www.kimshare.club:7001/sendEmail', params).then(res => {
+  return request.POST('http://www.51sjqt.com/:7001/sendEmail', params).then(res => {
     return res;
   })
 }
